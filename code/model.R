fe6d39f39276a4b1a3ae14d81017e4a43a7cd071
benbrew <benmbrew@gmail.com>
2015-02-15 14:55:24 +0100

    recent

--- code/model.R
+++ code/model.R
@@ -21,10 +21,10 @@ tony$School[1]
 
 #add county to to school in both data frames 
 
-school$School <- paste0(school$district, sep=" ", school$school, collapse=NULL)
+school$School <- paste0(school$school, sep=" ", school$district, collapse=NULL)
 
 
-tony$School <-paste0(tony$county, sep=" ",tony$School, collapse=NULL)
+tony$School <-paste0(tony$School, sep=" ",tony$county, collapse=NULL)
 
 
 #make column lower case
@@ -34,10 +34,10 @@ tony$School <- tolower(tony$School)
 
 #remove common words withing school
 
-school$School <- gsub("elementary", "", school$School)
-school$School <- gsub("elem", "", school$School)
-school$School <- gsub("middle", "", school$School)
-school$School <- gsub("high", "", school$School)
+school$School <- gsub("elementary", "elem", school$School)
+school$School <- gsub("elem", "elem", school$School)
+school$School <- gsub("middle", "mid", school$School)
+school$School <- gsub("high", "high", school$School)
 school$School <- gsub("school", "", school$School)
 school$School <- gsub("schl", "", school$School)
 school$School <- gsub("sch", "", school$School)
@@ -47,12 +47,14 @@ school$School <- gsub("[.]", "", school$School)
 school$School <- gsub(",", "", school$School)
 school$School <- gsub("/", "", school$School)
 school$School <- gsub("-", "", school$School)
+school$School <- gsub(" s ", "", school$School)
+school$School <- gsub(" oo ", "", school$School)
 
 
-tony$School <- gsub("elementary", "", tony$School)
-tony$School <- gsub("elem", "", tony$School)
-tony$School <- gsub("middle", "", tony$School)
-tony$School <- gsub("high", "", tony$School)
+tony$School <- gsub("elementary", "elem", tony$School)
+tony$School <- gsub("elem", "elem", tony$School)
+tony$School <- gsub("middle", "mid", tony$School)
+tony$School <- gsub("high", "high", tony$School)
 tony$School <- gsub("school", "", tony$School)
 tony$School <- gsub("schl", "", tony$School)
 tony$School <- gsub("sch", "", tony$School)
@@ -62,7 +64,19 @@ tony$School <- gsub("[.]", "", tony$School)
 tony$School <- gsub(",", "", tony$School)
 tony$School <- gsub("/", "", tony$School)
 tony$School <- gsub("-", "", tony$School)
-
+tony$School <- gsub(" es", "", tony$School)
+tony$School <- gsub(" ms", "", tony$School)
+tony$School <- gsub(" hs ", "", tony$School)
+tony$School <- gsub(" ak ", "", tony$School)
+tony$School <- gsub(" r ", "", tony$School)
+tony$School <- gsub(" ss ", "", tony$School)
+tony$School <- gsub(" l ", "", tony$School)
+tony$School <- gsub(" jh ", "", tony$School)
+tony$School <- gsub(" ld ", "", tony$School)
+tony$School <- gsub(" oj ", "", tony$School)
+tony$School <- gsub(" c ", "", tony$School)
+tony$School <- gsub(" e ", "", tony$School)
+tony$School <- gsub(" 5 ", "", tony$School)
 #try first row again
 # temp <- tony$School[1] #later replace 1 with i. 
 # mat <- adist(x=temp, y=school$School)
@@ -165,17 +179,29 @@ tony$School <- gsub("-", "", tony$School)
 # #I think it's good### now the loop
 
 tony$name <- NA
+#for(i in 1:nrow(tony)){
+  
+ # temp <- tony$School[i]  
+ # mat <- adist(x=temp, y=school$School)
+  
+  #ind <-which.min(mat)
+  
+  #tony$name[i] <- school$School[ind]
+#}
+
+# Compare School and name in tony
+
 for(i in 1:nrow(tony)){
   
   temp <- tony$School[i]  
-  mat <- adist(x=temp, y=school$School)
+  posibs <- school$School
+  posibs <- posibs[which(substr(posibs, 1, 1) == substr(temp, 1, 1))] 
+  mat <- adist(x=temp, y=posibs)
   
   ind <-which.min(mat)
   
-  tony$name[i] <- school$School[ind]
+  tony$name[i] <- posibs[ind]
 }
-
-# Compare School and name in tony
 tony[,c("School", "name")]
 
 #make new colummn school
