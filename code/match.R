c038f89afff4fc21a64023e51447172f63f89c11
joebrew <joebrew@gmail.com>
2015-02-18 09:27:36 -0500

    predictions

--- code/match.R
+++ code/match.R
@@ -18,9 +18,9 @@ if(Sys.info()['user'] == 'joebrew'){
 # READ IN TONY AND FDOE DATA
 #####
 setwd(public)
-school <- read.csv("public_schools.csv")
+school <- read.csv("public_schools.csv", stringsAsFactors = FALSE)
 setwd(private)
-tony <- read.csv("merged.csv")
+tony <- read.csv("merged.csv", stringsAsFactors = FALSE)
 
 #####
 # CLEAN UP DATAFRAME
@@ -29,6 +29,11 @@ tony <- read.csv("merged.csv")
 # Make lowercase columnnames in tony
 names(tony) <- tolower(names(tony))
 
+# Fix percent vaccinated in tony
+tony$percent_vaccinated <- 
+  as.numeric(
+    gsub("%", "", tony$percent_vaccinated))
+
 # Make lowercase district / county names
 school$district <- tolower(school$district)
 tony$district <- tolower(tony$county)
@@ -36,9 +41,17 @@ tony$district <- tolower(tony$county)
 # Remove periods from district names
 school$district <- gsub('[.]', '', school$district)
 
+#make column lower case
+school$school <- tolower(school$school)
+tony$school <- tolower(tony$school)
+
 # Remove trailing/leading whitespaces from all columns
 remove_trail <- function(var){
-  x <- gsub("^\\s+|\\s+$", "", as.character(var))
+  if(is.numeric(var)){
+    x <- as.numeric(gsub("^\\s+|\\s+$", "", as.character(var)))
+  } else {
+    x <- gsub("^\\s+|\\s+$", "", as.character(var))
+  }
   return(x)
 }
 
@@ -51,26 +64,16 @@ for (i in 1:ncol(school)){
 }
 
 
-# Paste together school and district name
-# school$school <- paste0(school$school, sep=" ", school$district, collapse=NULL)
-# tony$school <-paste0(tony$school, sep=" ",tony$county, collapse=NULL)
-
-#make column lower case
-school$school <- tolower(school$school)
-tony$school <- tolower(tony$school)
-
 #remove common words withing school
 remove_junk <- function(df){
-  df$school <- gsub("elementary", "es", df$school)
-  df$school <- gsub("middle", "ms", df$school)
-  df$school <- gsub("js", "ms", df$school)
-  df$school <- gsub("high", "hs", df$school)
   df$school <- gsub("school", "", df$school)
   df$school <- gsub("schl ", "", df$school)
+  df$school <- gsub("schl", "", df$school)
+  df$school <- gsub("center", "", df$school)
   df$school <- gsub("youth", "", df$school)
   df$school <- gsub("academy", "", df$school)
   df$school <- gsub("senior", "", df$school)
-  df$school <- gsub("sch ", "", df$school)
+  df$school <- gsub("sch$", "", df$school)
   df$school <- gsub("scho ", "", df$school)
   df$school <- gsub("program", "", df$school)
   df$school <- gsub("prog ", "", df$school)
@@ -82,6 +85,7 @@ remove_junk <- function(df){
   df$school <- gsub(" oo ", "", df$school)
   df$school <- gsub("   ", " ", df$school)
   df$school <- gsub("  ", " ", df$school)
+  df$school <- gsub("[(]|[)]", "", df$school)
   df$school <- remove_trail(df$school)
   
   return(df)
@@ -90,20 +94,33 @@ remove_junk <- function(df){
 school <- remove_junk(school)
 tony <- remove_junk(tony)
 
+
+# try our best to guess school age group from tony's school names
+tony$type_t <- ifelse(
+  grepl(" elem$| es$| elementary$| primary$| k-8$| k6$| elemtary$| k8$| elemetary$| elemenary$", tony$school),
+  "elem",
+  ifelse(grepl(" mid$| middle$| ms$| jh$| junior$| intermediate$", tony$school),
+         "mid",
+         ifelse(grepl(" high$| hs$| upper$| prep$", tony$school),
+                "high",
+                ifelse(grepl(" pk$| prek$| pre-k$", tony$school),
+                       "pre-k",
+                       NA))))
+
+# Clean up a bit more
+clean_again <- function(df){
+    df$school <- gsub("elementary", "es", df$school)
+    df$school <- gsub("middle", "ms", df$school)
+    df$school <- gsub("js", "ms", df$school)
+    df$school <- gsub("high", "hs", df$school)
+    return(df)
+}
+school <- clean_again(school); tony <- clean_again(tony)
+
+
 #####
 # LOOP THROUGH EACH NAME OF TONY TO GET BEST MATCH IN SCHOOL
 #####
-
-
-
-# OLD WAY
-#for(i in 1:nrow(tony)){
- # temp <- tony$school[i]  
- # mat <- adist(x=temp, y=school$school)
-  #ind <-which.min(mat)
-  #tony$name[i] <- school$school[ind]
-#}
-
 
 #make new name holder for tony
 tony$name <- NA
@@ -155,15 +172,6 @@ hist(tony$match_score) # most are great!
 tally(group_by(tbl_df(tony), match_score)) 
 table(is.na(tony$match_score)) # a few couldn't get any match at all
 
-# Look at different match scores to decide what the cutoff should be
-# the lower the better match
-tony[which(tony$match_score <= 1),c("school", "name")] # perfect
-tony[which(tony$match_score == 2),c("school", "name")] # pretty close to perfect
-tony[which(tony$match_score == 3),c("school", "name")] # good
-tony[which(tony$match_score == 4),c("school", "name")] # decent
-tony[which(tony$match_score == 5),c("school", "name")] # mediocre
-tony[which(tony$match_score >=10),c("school", "name")] # terrible
-
 # Let's keep only those with a match_score of less than
 tony_small <- tony[which(tony$match_score <= 1),]
 
@@ -174,30 +182,14 @@ final <- left_join(x=tony_small,
                    by="name")
 
 # Clean up final
-final <- final[,c("school.x", "name", "school_number", "totmem", "type", "year",
+final <- final[,c("school.x", "name", "school_number", "totmem", "type", "type_t", "year",
                   "total_black", "total_hispanic", "total_asian", "free_reduced", "per_fr",
                   "number_vaccinated", "number_enrolled", "percent_vaccinated", "county", "match_score")]
 
 # Remove 2013 and duplicate rows
+final <- final[order(final$match_score),]
 final <- final[which(final$year == 2014),]
 final <- final[!duplicated(final$name),]
-
-#build a model and then predict.
-
-#change to numeric 
-
-str(final)
-
-final$totmem <- as.numeric(final$totmem)
-final$total_black <- as.numeric(final$total_black)
-final$total_asian <- as.numeric(final$total_asian)
-final$total_hispanic <- as.numeric(final$total_hispanic)
-final$free_reduced <- as.numeric(final$free_reduced)
-final$per_fr <- as.numeric(final$per_fr)
-final$number_vaccinated <- as.numeric(final$number_vaccinated)
-final$number_enrolled <- as.numeric(final$number_enrolled)
-final$percent_vaccinated <- gsub("%", "", final$percent_vaccinated)
-final$percent_vaccinated <- as.numeric(final$percent_vaccinated)
 
 #new columns for percent black, percent asian, percent hispanic
 final$per_black <- (final$total_black/final$totmem)*100
@@ -207,16 +199,32 @@ final$per_black <- round(final$per_black, 2)
 final$per_asian <- round(final$per_asian, 2)
 final$per_hispanic <- round(final$per_hispanic, 2)
 
-#write new csv
-names(final)[1] <- "name_from_tony"
-final <- data.frame(final)
+school$per_black <- (school$total_black/school$totmem)*100
+school$per_asian <- (school$total_asian/school$totmem)*100
+school$per_hispanic <- (school$total_hispanic/school$totmem)*100
+school$per_black <- round(school$per_black, 2)
+school$per_asian <- round(school$per_asian, 2)
+school$per_hispanic <- round(school$per_hispanic, 2)
 
-#basic stats and charts for tony
-head(final)
+# fix first column name
+names(final)[1] <- "name_from_tony"
+
+# Create an imm rate based on school population (rather than tony's denoms)
+final$imm_rate <- final$number_vaccinated / final$totmem * 100
+
+# Look at correlation between our calculated imm rate and tony's
+plot(final$imm_rate, final$percent_vaccinated)
+
+# Let's remove any observations that are more than 15 percent off from tony's
+final <- final[which(sqrt((final$imm_rate - final$percent_vaccinated)^2) < 10),]
+
+# Replot
+plot(final$imm_rate, final$percent_vaccinated)
+
 
 #make chart for avg immunization for type of grade (elem, mid, high) 
-by_type <- final %>%
-  group_by(type) %>% 
+by_type <- tony %>%
+  group_by(type_t) %>% 
   summarise(avg_vac = mean(percent_vaccinated, na.rm =TRUE))
 
 #get rid of NA 
@@ -227,7 +235,7 @@ by_type <- arrange(by_type, avg_vac)
 
 #barplot by type of grade and avg vaccination achieved 
 bp <- barplot(by_type$avg_vac,
-        names.arg = by_type$type,
+        names.arg = by_type$type_t,
         col="lightblue",
         ylab = "Immunization rate",
         ylim = c(0, max(by_type$avg_vac) * 1.1),
@@ -250,9 +258,18 @@ plot(final$totmem, final$percent_vaccinated,
      pch=16,
      col=adjustcolor("lightblue", alpha.f=0.4))
 abline(lm(final$percent_vaccinated ~ final$totmem))
+#ADD LOESS LINE
+lox <- final$totmem
+loy <- final$percent_vaccinated
+lw1 <- loess(loy ~ lox, span=0.75)
+j <- order(lox)
+lines(lox[j],lw1$fitted[j],col=adjustcolor("red", alpha.f = 0.4),
+      lty=1,
+      lwd = 3)
+
 
 #plot county and average immunization, using tony's larger dataset
-by_county <- final %>% 
+by_county <- tony %>% 
   group_by(county) %>%
   summarise(avg_vac = mean(percent_vaccinated, na.rm=TRUE))
 
@@ -261,7 +278,7 @@ by_county <- final %>%
 by_county <- arrange(by_county, avg_vac)
 
 #how do I fit the names onto the X axis?
-cols <- adjustcolor(colorRampPalette(c("lightgreen", "green", "blue"))(nrow(by_county)), alpha.f = 0.5)
+cols <- adjustcolor(colorRampPalette(c("yellow", "blue"))(nrow(by_county)), alpha.f = 0.5)
 bp <- barplot(by_county$avg_vac,
         names.arg = by_county$county,
         cex.names = 0.7,
@@ -272,8 +289,8 @@ text(x = bp[,1],
      y = by_county$avg_vac,
      pos = 3,
      labels = paste0(round(by_county$avg_vac, digits = 1), "%"),
-     cex = 0.4,
-     col = cols)
+     cex = 0.7,
+     col = adjustcolor("black", alpha.f = 0.7))
 box("plot")
 abline(h = seq(0,30, 2), col = adjustcolor("black", alpha.f = 0.2), lty = 3)
 title(main = "Immunization Rate by County (Matched Schools)")
@@ -330,9 +347,78 @@ plot(final$per_hispanic, final$percent_vaccinated,
 
 #basic linear model 
 
-mod <- lm(percent_vaccinated ~ totmem + type + per_black +
-            per_hispanic + per_asian + per_fr + county, data=final)
+mod <- lm(number_vaccinated ~ 
+            totmem + 
+            type + 
+            per_black +
+            #per_hispanic + 
+            #per_asian + 
+            per_fr, 
+          data=final)
 summary(mod)
+
+# MAKE VISUALS OF PREDICTIONS
+fake <- expand.grid(totmem = 1000,
+                    type = unique(final$type),
+                    per_black = 0:100,
+                    per_fr = 0:100)
+fake$predicted <- predict(mod, newdata = fake)
+fake_int <- data.frame(predict(object = mod,
+                               interval = "prediction",
+                               newdata = fake,
+                               level = 0.95))
+fake$lwr <- fake_int$lwr
+fake$upr <- fake_int$upr
+
+# Plot fake predictions
+plot_fake <- function(per_black = 20,
+                      type = "elem",
+                      col = "red",
+                      lwd = 3, 
+                      add = FALSE,
+                      ylim = c(0, 200)){
+  col <- adjustcolor(col, alpha.f = 0.6)
+  df <- fake[which(fake$per_black == per_black &
+                     fake$type == type),]
+  if(add){
+    lines(df$per_fr, df$predicted, type = "l", col = col, lwd = lwd)
+    lines(df$per_fr, df$lwr, col = col, lwd = 1, lty = 3)
+    lines(df$per_fr, df$upr, col = col, lwd = 1, lty = 3)
+    
+  } else{
+    plot(df$per_fr, df$predicted, type = "l", col = col, lwd = lwd,
+         ylim = ylim)
+    lines(df$per_fr, df$lwr, col = col, lwd = 1, lty = 3)
+    lines(df$per_fr, df$upr, col = col, lwd = 1, lty = 3)
+    
+  }
+}
+plot_fake()
+plot_fake(type = "mid", add = TRUE, col = "green")
+plot_fake(type = "high", add = TRUE, col = "blue")
+
+# PREDICT OVER ALL THE SCHOOLS
+school$predicted <- predict(mod, newdata = school)
+school_int <- data.frame(predict(object = mod,
+                               interval = "prediction",
+                               newdata = school,
+                               level = 0.95))
+school$lwr <- school_int$lwr
+school$upr <- school_int$upr
+
+# Create predicted RATES
+school$predicted_rate <- school$predicted / school$totmem * 100
+school$lwr_rate <- school$lwr / school$totmem * 100
+school$upr_rate <- school$upr / school$totmem * 100
+
+# Order schools by predicted imm rate
+school <- school[rev(order(school$predicted_rate)),]
+
+# Remove NAs
+school <- school[which(!is.na(school$predicted_rate)),]
+
+# Remove those with screwy denominators (ie, predicted rate greater than denom)
+school <- school[which(school$predicted_rate < 100),]
 
 #predict 
 
@@ -349,7 +435,6 @@ prediction_intervals <- data.frame(predict(object = mod,
                                            level = 0.95))
 
 #create columns for lower and upper bound of confidence intervals
-
 final$lwr <- prediction_intervals$lwr
 final$upr <- prediction_intervals$upr
 
